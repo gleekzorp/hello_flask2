9398685e18cb4a1d54a2037b1fb6db3db1858216
Daniel <gleek.dan@gmail.com>
2021-01-22 16:46:27 -0700

    add post route to create a single guide

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -11,22 +11,38 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 class Guide(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=False)
-    content = db.Column(db.String(144), unique=False)
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), unique=False)
+  content = db.Column(db.String(144), unique=False)
 
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
+  def __init__(self, title, content):
+    self.title = title
+    self.content = content
+
 
 class GuideSchema(ma.Schema):
-    class Meta:
-        fields = ('title', 'content')
+  class Meta:
+    fields = ('title', 'content')
 
 
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
+# Endpoint to create a new guide
+@app.route('/guide', methods=["POST"])
+def add_guide():
+  title = request.json['title']
+  content = request.json['content']
+
+  new_guide = Guide(title, content)
+
+  db.session.add(new_guide)
+  db.session.commit()
+
+  guide = Guide.query.get(new_guide.id)
+
+  return guide_schema.jsonify(guide)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
